cbbbcd8e56feeb2bfc2fa532ef5184cb7a44b798
plancoach <plancoach1352@gmail.com>
2023-09-10 18:05:33 +0900

    20230910 version 3.25

--- consultapp/decorators.py
+++ consultapp/decorators.py
@@ -108,7 +108,7 @@ def ConsultPaymentListDecorator(func):
         decorators.update()
         permission_checks = [
             decorators.object_filter(allow_object= 'all'),
-            decorators.member_filter(role='student', allow_superuser=True),
+            decorators.member_filter(role='member', allow_superuser=True),
         ]
         for check in permission_checks:
             if check is not None:
